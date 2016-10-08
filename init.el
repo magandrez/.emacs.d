0059752217cc0268d523e85813f117812221ecf5
Manuel González <magandrez@gmail.com>
2016-10-08 11:16:51 +0300

    Uses CURL cask.
    Brew's cask installation crashed emacs on init (did not find pallet). Switched to CURL installed cask solves the issue.

--- init.el
+++ init.el
@@ -1,4 +1,11 @@
-(require 'cask "/usr/local/Cellar/cask/0.8.0/cask.el")
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(require 'cask "~/.cask/cask.el")
 (cask-initialize)
 (require 'pallet)
 (pallet-mode t)
