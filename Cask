b6536c2667798020feb7e6f05cbc200406012797
Manuel González <magandrez@gmail.com>
2016-10-09 12:53:05 +0300

    Adds basic auto-complete functionality

--- Cask
+++ Cask
@@ -1,3 +1,4 @@
 (source melpa)
 
+(depends-on "auto-complete")
 (depends-on "pallet")
---
+++ configs/auto-complete.el
@@ -0,0 +1,4 @@
+(add-to-list 'ac-dictionary-directories
+     "~/.emacs.d/.cask/26.0/elpa/auto-complete-20160827.649/dict")
+(ac-config-default)
+(setq ac-ignore-case nil)
