5517577f61d72bd2b563c7cae1999e42736542dc
Manuel González <magandrez@gmail.com>
2016-10-16 13:33:57 +0300

    Adds integration between Emacs and Ruby Version Manager

--- Cask
+++ Cask
@@ -1,5 +1,8 @@
+(source gnu)
+(source marmalade)
 (source melpa)
 
 (depends-on "ag")
 (depends-on "auto-complete")
 (depends-on "pallet")
+(depends-on "rvm")
--- init.el
+++ init.el
@@ -37,7 +37,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (ag pallet auto-complete async))))
+ '(package-selected-packages (quote (rvm ag pallet auto-complete async))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
