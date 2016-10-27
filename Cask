389f09852a346c21f96a1b2d54273769eed21e78
Manuel González <magandrez@gmail.com>
2016-10-27 19:33:24 +0300

    Adds enhanced ruby mode and robe hook

--- Cask
+++ Cask
@@ -4,6 +4,7 @@
 
 (depends-on "ag")
 (depends-on "auto-complete")
+(depends-on "enh-ruby-mode")
 (depends-on "pallet")
 (depends-on "robe")
 (depends-on "rvm")
--- configs/ruby.el
+++ configs/ruby.el
@@ -1,2 +1,5 @@
 (defadvice inf-ruby-console-auto (before activate-rvm-for-robe activate)
   (rvm-activate-corresponding-ruby))
+
+(add-to-list 'auto-mode-alist
+             '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))
