b9243ec01faf995399a5e4d0c42ab4896aceed22
Manuel Gonzalez <manuel.gonzalez@foodie.fm>
2016-11-07 13:49:48 +0200

    Common config modifications
    - Enables META key (to use {} with Ctrl+Alt+( or Ctrl+Alt+).
    - Enables ZSH as Emacs terminal.
    - Automatically reloads open buffers.
    - Commented common.el

--- configs/common.el
+++ configs/common.el
@@ -1,2 +1,16 @@
+;; Hook for Robe to play with Enhanced Ruby Mode
 (add-hook 'enh-ruby-mode-hook 'robe-mode)
+
+;; Monokai theme
 (load-theme 'monokai t)
+
+;; ZSH as shell
+(setenv "ESHELL" (expand-file-name "~/bin/eshell"))
+
+;; Enable {}
+(setq mac-option-modifier nil
+      mac-command-modifier 'meta
+      x-select-enable-clipboard t)
+
+;; Automatically reload a file iff changed
+(global-auto-revert-mode 1)
