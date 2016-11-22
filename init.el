cbf423c7e8a184399362a7b9a796c8d2ee3b3ce7
Manuel González <magandrez@gmail.com>
2016-11-22 16:37:15 +0200

    Remove prompt when creating unnamed buffer.

--- init.el
+++ init.el
@@ -37,6 +37,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ido-create-new-buffer (quote always))
  '(inhibit-startup-screen t)
  '(initial-scratch-message nil)
  '(menu-bar-mode nil)
