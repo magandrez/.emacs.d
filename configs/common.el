ce65c7b6a382597ea55ceb0c07e492f3f412b01a
Manuel Gonzalez <manuel.gonzalez@foodie.fm>
2016-11-24 15:46:15 +0200

    Stop littering everywhere and centralise backups.

--- configs/common.el
+++ configs/common.el
@@ -33,3 +33,11 @@
 
 (global-set-key (kbd "C-x r") 'reload-this-buffer)
 (global-set-key (kbd "M-o") 'other-window)
+
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+  backup-by-copying t    ; Don't delink hardlinks
+  version-control t      ; Use version numbers on backups
+  delete-old-versions t  ; Automatically delete excess backups
+  kept-new-versions 10   ; how many of the newest versions to keep
+  kept-old-versions 5    ; and how many of the old
+  )
