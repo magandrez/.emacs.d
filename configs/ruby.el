1a25f11a4572537edff2080a86cbd9dfd2a50243
Manuel Gonzalez <manuel.gonzalez@foodie.fm>
2017-01-08 14:02:04 +0200

    Adds Capistrano file type to enh-ruby-mode known list
    This is needed in case of creating Capistrano 3 tasks.

--- configs/ruby.el
+++ configs/ruby.el
@@ -2,4 +2,4 @@
   (rvm-activate-corresponding-ruby))
 
 (add-to-list 'auto-mode-alist
-             '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))
+             '("\\(?:\\.rb\\|cap\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))
