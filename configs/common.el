ed1f592082b9851f88d5a892178dc55b568eafda
Manuel Gonzalez <manuel.gonzalez@foodie.fm>
2017-01-23 17:09:07 +0200

    Support for sudo (tramp mode)

--- configs/common.el
+++ configs/common.el
@@ -71,3 +71,17 @@
 ;; Enable yafolding (fold code based on indentation)
 (add-hook 'prog-mode-hook
           (lambda () (yafolding-mode)))
+
+;; Tramp as sudo
+(defun find-alternative-file-with-sudo ()
+  (interactive)
+  (let ((fname (or buffer-file-name
+		   dired-directory)))
+    (when fname
+      (if (string-match "^/sudo:root@localhost:" fname)
+	  (setq fname (replace-regexp-in-string
+		       "^/sudo:root@localhost:" ""
+		       fname))
+	(setq fname (concat "/sudo:root@localhost:" fname)))
+      (find-alternate-file fname))))
+
