617255f6e50af8ef9091ad0daa9a90489dc18176
Manuel Gonzalez <manuel.gonzalez@foodie.fm>
2017-05-13 18:40:20 +0300

    Dired find open in other buffer

--- init.el
+++ init.el
@@ -85,3 +85,5 @@
 (tramp-set-completion-function "ssh"
                   '((tramp-parse-sconfig "/etc/ssh_config")
                     (tramp-parse-sconfig "~/.ssh/config")))
+
+(put 'dired-find-alternate-file 'disabled t)
