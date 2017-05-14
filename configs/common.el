6e87254f68fc146f77f6ae2228415f93770fcc08
Manuel González <magandrez@gmail.com>
2017-05-14 12:37:30 +0300

    Sets org mode's todo list to track timestamps.

--- configs/common.el
+++ configs/common.el
@@ -93,4 +93,6 @@
 (add-hook 'prog-mode-hook 'column-enforce-mode)
 
 ;; Org mode configs
- '(org-log-done (quote note))
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
