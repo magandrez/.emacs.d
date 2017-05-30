f60ca650fe9d45dc309a824e7066c75010eb9ffa
Manuel González <manuel.gonzalez@foodie.fm>
2017-05-30 14:56:58 +0300

    Better specifying support for all Ruby-related files

--- configs/ruby.el
+++ configs/ruby.el
@@ -11,8 +11,13 @@
 (defadvice inf-ruby-console-auto (before activate-rvm-for-robe activate)
   (rvm-activate-corresponding-ruby))
 
-(add-to-list 'auto-mode-alist
-             '("\\(?:\\.rb\\|cap\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . enh-ruby-mode))
 
 (autoload 'auto-make-header "header2")
 (add-hook 'enh-ruby-mode 'auto-make-header)
