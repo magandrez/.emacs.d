75b70ac7d16364b44c4b9b8b83269f6141bcecc4
Manuel Gonzalez <manuel.gonzalez@foodie.fm>
2018-01-31 16:46:58 +0200

    Fixes go-mode configs

--- configs/go.el
+++ configs/go.el
@@ -1,21 +1,14 @@
-(when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize)
-  (exec-path-from-shell-copy-env "GOPATH"))
-
+(setq gofmt-command "goimports")
 (defun my-go-mode-hook ()
   (add-hook 'before-save-hook 'gofmt-before-save) ; gofmt before every save
   ; Godef jump key binding                                                      
   (local-set-key (kbd "M-.") 'godef-jump)
   (local-set-key (kbd "M-*") 'pop-tag-mark)
+  (local-set-key (kbd "M-p") 'compile)            ; Invoke compiler
+  (local-set-key (kbd "M-P") 'recompile)          ; Redo most recent compile cmd
+  (local-set-key (kbd "M-]") 'next-error)         ; Go to next error (or msg)
+  (local-set-key (kbd "M-[") 'previous-error)     ; Go to previous error or msg
   )
 (add-hook 'go-mode-hook 'my-go-mode-hook)
 
-(local-set-key (kbd "M-p") 'compile)            ; Invoke compiler
-(local-set-key (kbd "M-P") 'recompile)          ; Redo most recent compile cmd
-(local-set-key (kbd "M-]") 'next-error)         ; Go to next error (or msg)
-(local-set-key (kbd "M-[") 'previous-error)     ; Go to previous error or msg
 
-(auto-complete-mode 1)
-
-(setq gofmt-command "goimports")
-(define-key ac-mode-map (kbd "M-f") 'auto-complete)
