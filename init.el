12f31b81f511e094425e7231d1ee55ecc325f038
Manuel Gonzalez <manuel.gonzalez@foodie.fm>
2018-01-31 16:45:35 +0200

    Adds go-playground package

--- init.el
+++ init.el
@@ -57,7 +57,7 @@
  '(menu-bar-mode nil)
  '(package-selected-packages
    (quote
-    (calfw-org calfw org-gcal org-journal header2 suomalainen-kalenteri ace-window org-present password-store column-enforce-mode markdown-mode flycheck flymake-go go-autocomplete go-mode yaml-mode multi-term company-quickhelp company magit monokai-theme enh-ruby-mode robe rvm ag pallet auto-complete async)))
+    (go-playground-cli calfw-org calfw org-gcal org-journal header2 suomalainen-kalenteri ace-window org-present password-store column-enforce-mode markdown-mode flycheck flymake-go go-autocomplete go-mode yaml-mode multi-term company-quickhelp company magit monokai-theme enh-ruby-mode robe rvm ag pallet auto-complete async)))
  '(safe-local-variable-values
    (quote
     ((encoding . utf-8)
