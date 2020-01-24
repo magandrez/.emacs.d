5148c259fcf4ae969ac6cd99f927faa57f1b1f27
Manuel González <manuel.gonzalez@foodie.fm>
2020-01-24 16:25:19 +0200

    Remove non-sensible gc-threshold value
    More info: https://www.reddit.com/r/emacs/comments/8rbl63/emacs26_is_very_slow_when_viewing_large_org_files/e0rn4v8\?utm_source\=share\&utm_medium\=web2x
    -  https://bling.github.io/blog/2016/01/18/why-are-you-changing-gc-cons-threshold/

--- init.el
+++ init.el
@@ -1,26 +1,23 @@
-(let ((gc-cons-threshold most-positive-fixnum))
+;; Set repositories
+(require 'package)
+(setq-default
+ load-prefer-newer t
+ package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(setq package-enable-at-startup nil)
+(package-initialize)
 
-  ;; Set repositories
-  (require 'package)
-  (setq-default
-   load-prefer-newer t
-   package-enable-at-startup nil)
-  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-  (setq package-enable-at-startup nil)
-  (package-initialize)
+;; Install dependencies
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package t))
+(setq-default
+ use-package-always-defer t
+ use-package-always-ensure t)
 
-  ;; Install dependencies
-  (unless (package-installed-p 'use-package)
-    (package-refresh-contents)
-    (package-install 'use-package t))
-  (setq-default
-   use-package-always-defer t
-   use-package-always-ensure t)
+;; Use latest Org
+(use-package org :ensure org-plus-contrib)
 
-  ;; Use latest Org
-  (use-package org :ensure org-plus-contrib)
-
-  ;; Tangle configuration
-  (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
-(garbage-collect))
+;; Tangle configuration
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
