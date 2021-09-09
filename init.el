33e2b0997b0264832d206e0c22206ae314841c16
Manuel González <manuel@manuel.is>
2021-09-09 11:55:46 +0300

    fix: Init after Org-mode changes
    More info:
        https://orgmode.org/elpa.html
        https://orgmode.org/list/87ft63vfwc.fsf@bzg.fr/
        https://orgmode.org/list/87a6qnixnr.fsf@gnu.org/
        https://orgmode.org/list/87blb3epey.fsf@gnu.org/

--- init.el
+++ init.el
@@ -1,25 +1,24 @@
 ;; Set repositories
 (require 'package)
 (setq-default
- load-prefer-newer t
- package-enable-at-startup nil)
+load-prefer-newer t
+package-enable-at-startup nil)
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
 (package-initialize)
 
 ;; Install dependencies
 (unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package t))
+(package-refresh-contents)
+(package-install 'use-package t))
 (setq-default
- use-package-always-defer t
- use-package-always-ensure t)
+use-package-always-defer t
+use-package-always-ensure t)
 
 ;; Use latest Org
-(use-package org :ensure org-plus-contrib)
+(use-package org :ensure org-contrib)
 
 ;; Tangle configuration
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
