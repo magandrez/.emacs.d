835b2f59819ccbe41471b94bf19525b5439cca1a
Manuel González <manuel@manuel.is>
2022-12-16 20:05:40 +0200

    Update init.el

--- init.el
+++ init.el
@@ -1,9 +1,14 @@
+;;; init.el --- Setting up the basics
+;;; Commentary:
+;;; This template init.el setups the basic config that enable
+;;; Tangling config.org file into config.el
 ;; Set repositories
 (require 'package)
+;;; Code:
 (setq-default
 load-prefer-newer t
 package-enable-at-startup nil)
-(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+(setq-default gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
