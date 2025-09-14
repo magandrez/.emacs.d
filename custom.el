f4d9c14d99500bcb1864309faba5389b9e4360d3
Manuel González <manuel@manuel.is>
2025-09-14 20:01:27 +0200

    Remove unnecessary customizations from custom.el

--- custom.el
+++ custom.el
@@ -4,8 +4,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(global-display-fill-column-indicator-mode nil)
- '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
